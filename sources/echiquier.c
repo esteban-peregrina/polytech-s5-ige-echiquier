e183f9788fbccaa25033d0b891939914cbc3e962
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2024-11-28 11:42:04 +0000

    Add insertionListeCasesAtteignables() definition

--- sources/echiquier.c
+++ sources/echiquier.c
@@ -1,2 +1,17 @@
 #include ".././headers/piece.h"
 #include ".././headers/echiquier.h"
+
+
+#include <stdlib.h>
+
+void insertionListeCasesAtteignables(Case* caseAtteignable, ListeCasesAtteignables* listeCasesAtteignables) {
+    /*
+    Insère caseAtteignable en tete de listeCasesAtteignables
+    */
+
+    Case* temp = listeCasesAtteignables->tete;
+    if (temp != NULL) { temp->caseAtteignablePrecedente == NULL; }
+    listeCasesAtteignables->tete = caseAtteignable;
+    caseAtteignable->caseAtteignableSuivante = temp;
+    caseAtteignable->caseAtteignablePrecedente = NULL;
+}
