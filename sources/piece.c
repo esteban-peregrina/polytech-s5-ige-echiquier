0abc9bb5fbee55da17cd87fde0223fa2f804be3b
Compte invité <pltguest@pc4013.polytech.sorbonne-universite.fr>
2024-11-28 12:36:29 +0100

    Update piece.c, improve CalculAtteignablePion(), add CalculAtteignableTour()

--- sources/piece.c
+++ sources/piece.c
@@ -1,5 +1,51 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include ".././headers/echiquier.h"
+#include ".././headers/piece.h"
 
-void calculAtteignableRoi(Piece* self, Case Plateau[][]){
 
+
+void calculAtteignablePion(Piece* self, Case Plateau[8][8]){
+    int xPiece = self->x;
+    int yPiece = self->y;
+
+    if (self->couleur == BLANC){                                                                              //Déplacements classique
+        if(xPiece<8 && Plateau[xPiece+1][yPiece].piece==NULL){
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece], self->coupsAtteignables);
+        }
+        if(xPiece == 2){
+            if(Plateau[xPiece+2][yPiece].piece==NULL){                                                        //Avance de deux s'il est sur la ligne de départ
+                insertionListeCaseAtteignables(&Plateau[xPiece+2][yPiece], self->coupsAtteignables);
+            }
+        }
+        if(xPiece<8 && yPiece<8 && Plateau[xPiece+1][yPiece+1].piece!=NULL){                                  //Possibilité de manger en diagonale
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece+1], self->coupsAtteignables);
+        }
+        if(xPiece<8 && yPiece>1 && Plateau[xPiece+1][yPiece-1].piece!=NULL){
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece-1], self->coupsAtteignables);
+        }
+    }else{
+        if(xPiece >1 && Plateau[xPiece-1][yPiece].piece==NULL){
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece], self->coupsAtteignables);
+        }
+        if(xPiece == 7){
+            if(Plateau[xPiece-2][yPiece].piece==NULL){
+                insertionListeCaseAtteignables(&Plateau[xPiece-2][yPiece], self->coupsAtteignables);
+            }
+        }
+        if(xPiece>1 && yPiece<8 && Plateau[xPiece-1][yPiece+1].piece!=NULL){
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece+1], self->coupsAtteignables);
+        }
+        if(xPiece>1 && yPiece>1 && Plateau[xPiece+1][yPiece-1].piece!=NULL){
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece-1], self->coupsAtteignables);
+        }
+    }
+}
+
+void calculAtteignableTour(Piece* self, Case Plateau[8][8]){
+    int xPiece = self->x;
+    int yPiece = self->y;
+    while(xPiece>=1 && xPiece<=8 && yPiece>=1 && yPiece<=8 && Plateau[xPiece][yPiece].piece!=NULL ){
+
+    }
 }
