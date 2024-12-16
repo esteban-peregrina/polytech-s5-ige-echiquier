40e1d4ea2628730c8c73b115f993e9e4782374c2
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2024-12-16 15:03:57 +0000

    Update piece.c, update Piece attribute name

--- sources/piece.c
+++ sources/piece.c
@@ -11,33 +11,33 @@ void calculAtteignablePion(Piece* self, Case Plateau[8][8]){
 
     if (self->couleur == BLANC){                                                                              //Déplacements classique
         if(xPiece<8 && Plateau[xPiece+1][yPiece].piece==NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece], self->casesAtteignables);
         }
         if(xPiece == 2){
             if(Plateau[xPiece+2][yPiece].piece==NULL){                                                        //Avance de deux s'il est sur la ligne de départ
-                insertionListeCaseAtteignables(&Plateau[xPiece+2][yPiece], self->coupsAtteignables);
+                insertionListeCaseAtteignables(&Plateau[xPiece+2][yPiece], self->casesAtteignables);
             }
         }
         if(xPiece<8 && yPiece<8 && Plateau[xPiece+1][yPiece+1].piece!=NULL){                                  //Possibilité de manger en diagonale
-            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece+1], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece+1], self->casesAtteignables);
         }
         if(xPiece<8 && yPiece>1 && Plateau[xPiece+1][yPiece-1].piece!=NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece-1], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece+1][yPiece-1], self->casesAtteignables);
         }
     }else{
         if(xPiece >1 && Plateau[xPiece-1][yPiece].piece==NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece], self->casesAtteignables);
         }
         if(xPiece == 7){
             if(Plateau[xPiece-2][yPiece].piece==NULL){
-                insertionListeCaseAtteignables(&Plateau[xPiece-2][yPiece], self->coupsAtteignables);
+                insertionListeCaseAtteignables(&Plateau[xPiece-2][yPiece], self->casesAtteignables);
             }
         }
         if(xPiece>1 && yPiece<8 && Plateau[xPiece-1][yPiece+1].piece!=NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece+1], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece+1], self->casesAtteignables);
         }
         if(xPiece>1 && yPiece>1 && Plateau[xPiece+1][yPiece-1].piece!=NULL){
-            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece-1], self->coupsAtteignables);
+            insertionListeCaseAtteignables(&Plateau[xPiece-1][yPiece-1], self->casesAtteignables);
         }
     }
 }
