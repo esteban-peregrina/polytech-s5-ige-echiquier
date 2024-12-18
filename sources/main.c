e3307e5ce088b2f94df0f8be8def615dd88a142a
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2024-12-18 12:54:41 +0000

    FEAT: Add test for Plateau functions
    Update main() in main.c

--- sources/main.c
+++ sources/main.c
@@ -6,5 +6,13 @@
 #include ".././headers/echiquier.h"
 
 int main() {
+
+    // Test de l'affichage du Plateau vide
+    Case* Plateau[8][8]; // Déclaration du Plateau
+    initialisePlateau(Plateau); // Initialisation du plateau (chaque case est vide, non selectionnée ni atteignable, et de la bonne couleur)
+    affichePlateau(Plateau);
+    videPlateau(Plateau);
+
+    
     return 0;
 }
