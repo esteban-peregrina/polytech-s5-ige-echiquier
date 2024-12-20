0bb26079b1f6293c299321e8313487eaf413d44b
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2024-12-20 15:51:06 +0000

    FEAT: Add new libraries to configure terminal and change main() to test partieEchec()
    Update main.c

--- sources/main.c
+++ sources/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -6,20 +8,8 @@
 #include ".././headers/echiquier.h"
 
 int main() {
+    partieEchec();
 
-    // Test de l'affichage du Echiquier vide
-    Case* Echiquier[8][8]; // Déclaration du Echiquier
-    initialiseEchiquier(Echiquier); // Initialisation du Echiquier (chaque case est vide, non selectionnée ni atteignable, et de la bonne couleur)
-    Echiquier[0][1]->estAtteignable = true;
-    Echiquier[3][1]->estAtteignable = true;
-    Echiquier[0][0]->estSelectionne = true;
-
-    Piece* Joueurs[2];
-    initialiseJoueur(Joueurs, Echiquier);
-
-    afficheEchiquier(Echiquier);
-
-    videEchiquier(Echiquier);
-
+    printf("Programme terminé.\n");
     return 0;
 }
