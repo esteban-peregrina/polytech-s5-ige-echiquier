fc32f0e85d6c6ccc73bab3ccbb936914caf6fc5a
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2024-12-22 11:57:59 +0000

    REF: Rework calculAtteignable functions signature and add insertionCasesAtteignables() and actualiseCasesAtteignables() signatures
    Update piece.h according to Piece sruct change

--- headers/piece.h
+++ headers/piece.h
@@ -23,34 +23,34 @@ typedef struct Piece {
     int x;
     int y;      
 
-    void (*calculAtteignable)(struct Piece* self, Case* Plateau[8][8], struct Piece* Joueur[16]);
+    void (*calculAtteignable)(Case* Echiquier[8][8], struct Piece* Roi, struct Piece* self);
 
     struct Case* casesAtteignables[64];
     
     bool estSelectionnee;
+    bool estCapturee;
 
 } Piece;
 
 // Déclaration et suppresion de pièces
 Piece* creationPiece(Role role, int couleur);
-void destructionPiece(Piece* piece);
 
 // Opérations sur les pièces
-void calculAtteignablePion(Piece* self, Case* Echiquier[8][8], Piece* Joueurs[2]);
-void calculAtteignableTour(Piece* self, Case* Echiquier[8][8], Piece* Joueurs[2]);
-void calculAtteignableCavalier(Piece* self, Case* Echiquier[8][8], Piece* Joueurs[2]);
-void calculAtteignableFou(Piece* self, Case* Echiquier[8][8], Piece* Joueurs[2]);
-void calculAtteignableReine(Piece* self, Case* Echiquier[8][8], Piece* Joueurs[2]);
-void calculAtteignableRoi(Piece* self, Case* Echiquier[8][8], Piece* Joueurs[2]);
-void actualiseCasesAtteignables(Piece* piecePrecedente, Piece* pieceCourante, Case* Echiquier[8][8], Piece* Joueurs[2]); // Vide ListeCasesAtteignables de piecePrecedente et rempli celle de pieceCourante
+void calculAtteignablePion(Case* Echiquier[8][8], Piece* Roi, Piece* self);
+void calculAtteignableTour(Case* Echiquier[8][8], Piece* Roi, Piece* self);
+void calculAtteignableCavalier(Case* Echiquier[8][8], Piece* Roi, Piece* self);
+void calculAtteignableFou(Case* Echiquier[8][8], Piece* Roi, Piece* self);
+void calculAtteignableReine(Case* Echiquier[8][8], Piece* Roi, Piece* self);
+void calculAtteignableRoi(Case* Echiquier[8][8], Piece* Roi, Piece* self);
+
+void insertionCasesAtteignables(Piece* pieceCourante, Case* caseAtteignable);
+void actualiseCasesAtteignables(Case* Echiquier[8][8], Piece* Roi, Piece* pieceActualisable);
 
 // Opérations sur le Roi
 bool suisjeAtteignable(int xCible, int yCible, Case* Echiquier[8][8]);
 bool leaveRoi(Piece* self, int xCible, int yCible, Piece* Joueurs[2], Case* Echiquier[8][8]);
 
 // Opération sur les joueurs
-void insertionPieceJoueur(Piece* pieceAjoutable, Piece* Joueurs[2]);
-void supressionPieceJoueur(Piece* pieceRetirable, Piece* Joueurs[2]);
-void initialiseJoueur(Piece* Joueurs[2], Case* Echiquier[8][8]);
+void initialiseJoueur(Case* Echiquier[8][8], Piece* Joueur[16], int couleur);
 
 #endif // PIECE_H
