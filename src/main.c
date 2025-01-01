6cf9ebdad4f01597370e6cd6d4c4d1af3958679e
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2025-01-01 21:25:13 +0000

    REF: Just update todo-lists
    For reminder purpose

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include ".././include/echiquier.h"
 
-// TODO - Exit codes specifique
-// TODO - Revoir fonction de sauvegarde (taille entrées oui/non + SSI case capturée alors on la met dans l'echiquier)
-// ! - Sur main : erreur après sauvegarde : améliorer le code pour le simplifier et eviter les duplications
+// TODO - Test unitaire ?
+// TODO - Revoir fonction de sauvegarde (éviter les duplications)
+// TODO - Timer
+// TODO - IA
 
 int main() {
     jeuEchec();
